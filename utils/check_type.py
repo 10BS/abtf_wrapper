fd61dda61b69852c9d3216bfd38537b3bc582291
reue <reue5118@protonmail.com>
2024-09-13 19:24:36 +1000

    Deprecated

--- utils/check_type.py
+++
@@ -1,7 +0,0 @@
-from typing import Any
-
-
-class CheckType:
-    @staticmethod
-    def is_list_of(obj, of_: Any):
-        return isinstance(obj, list) & all(isinstance(item, of_) for item in obj)

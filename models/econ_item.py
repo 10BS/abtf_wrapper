90bf85fbf7b597a92853262ef4638fd6a3b22e77
reue <reue5118@protonmail.com>
2024-09-15 19:45:30 +1000

    -Optional

--- models/econ_item.py
+++ models/econ_item.py
@@ -1,16 +1,14 @@
-from typing import Optional
-
 from pydantic import BaseModel, Field
 
 
 class Description(BaseModel):
     value: str
-    color: Optional[str] = None
-    app_data: Optional[dict[str, int]] = None
+    color: str | None = None
+    app_data: dict[str, int] | None = None
 
 
 class Action(BaseModel):
-    link: Optional[str] = None
+    link: str | None = None
     name: str
 
 
@@ -18,10 +16,10 @@ class Tag(BaseModel):
     internal_name: str
     name: str
     category: str
-    color: Optional[str] = None
+    color: str | None = None
     category_name: str
-    localized_tag_name: Optional[str] = None
-    localized_category_name: Optional[str] = None
+    localized_tag_name: str | None = None
+    localized_category_name: str | None = None
 
 
 class FilterData(BaseModel):
@@ -30,48 +28,44 @@ class FilterData(BaseModel):
 
 
 class AppData(BaseModel):
-    quantity: Optional[int] = None
+    quantity: int | None = None
     def_index: int
     quality: int
-    limited: Optional[str] = None
-    slot: Optional[int] = None
-    filter_data: Optional[FilterData] = None
+    limited: str | None = None
+    slot: int | None = None
+    filter_data: FilterData | None = None
 
 
 class EconItem(BaseModel):
-    app_id: Optional[int] = Field(default=None, alias="appid")
-    context_id: Optional[int] = Field(default=None, alias="contextid")
-    asset_id: Optional[int] = Field(default=None, alias="assetid")
-    id: Optional[int] = None
-    class_id: Optional[int] = Field(default=None, alias="classid")
+    app_id: int | None = Field(default=None, alias="appid")
+    context_id: int | None = Field(default=None, alias="contextid")
+    asset_id: int | None = Field(default=None, alias="assetid")
+    id: int | None = None
+    class_id: int | None = Field(default=None, alias="classid")
     instance_id: int = Field(alias="instanceid")
-    amount: Optional[str] = None
-    pos: Optional[int] = None
-    missing: Optional[bool] = None
-    currency: Optional[int] = None
+    amount: str | None = None
+    pos: int | None = None
+    missing: bool | None = None
+    currency: int | None = None
     background_color: str
     icon_url: str
     icon_url_large: str
-    icon_drag_url: Optional[str] = None
+    icon_drag_url: str | None = None
     descriptions: list[Description]
     tradable: int
-    actions: Optional[list[Action]] = None
-    fraud_warnings: Optional[list[str]] = Field(default=None, alias="fraudwarnings")
+    actions: list[Action] | None = None
+    fraud_warnings: list[str] | None = Field(default=None, alias="fraudwarnings")
     name: str
     name_color: str
     type: str
     market_name: str
     market_hash_name: str
-    market_actions: Optional[list[Action]] = None
+    market_actions: list[Action] | None = None
     commodity: int
     market_tradable_restriction: int
     market_marketable_restriction: int
     marketable: int
     tags: list[Tag]
     app_data: AppData
-    owner_descriptions: Optional[list[str]] = None
-    owner_actions: Optional[list[str]] = None
-
-
-class EconItems(BaseModel):
-    econ_items: list[EconItem]
+    owner_descriptions: list[str] | None = None
+    owner_actions: list[str] | None = None

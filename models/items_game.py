62be44461e644c676d9db5a52a0f02262a438d05
reue <reue5118@protonmail.com>
2024-09-15 19:47:05 +1000

    -ClassVar; -Optional;

--- models/items_game.py
+++ models/items_game.py
@@ -1,38 +1,37 @@
 from datetime import datetime
-from typing import Optional, ClassVar
 
 from pydantic import BaseModel, Field
 
 
-class Rarity(BaseModel):
+class Rarity(BaseModel, extra="allow"):
     value: int
     loc_key: str
     loc_key_weapon: str
     color: str
-    loot_list: Optional[str]
-    drop_sound: Optional[str]
-    next_rarity: Optional[str]
+    loot_list: str | None = None
+    drop_sound: str | None = None
+    next_rarity: str | None = None
 
 
-class ConditionLogic(BaseModel):
+class ConditionLogic(BaseModel, extra="allow"):
     type: str
-    value: Optional[str] = None
-    player_key: Optional[str] = None
-    get_player: Optional[str] = None
-    is_owner: Optional[int] = None
-    team_key: Optional[str] = None
-    team_requirement: Optional[int] = None
-    distance_check_type: Optional[int] = None
-    distance_to_check: Optional[int] = None
-    key_to_lookup: Optional[str] = None
+    value: str | None = None
+    player_key: str | None = None
+    get_player: str | None = None
+    is_owner: int | None = None
+    team_key: str | None = None
+    team_requirement: int | None = None
+    distance_check_type: int | None = None
+    distance_to_check: int | None = None
+    key_to_lookup: str | None = None
 
 
-class Quest(BaseModel):
+class Quest(BaseModel, extra="allow"):
     name: str
     condition_logic: dict[str, dict[str, ConditionLogic]]
     type: str
-    event_name: Optional[str] = None
-    score_key_name: Optional[str] = None
+    event_name: str | None = None
+    score_key_name: str | None = None
 
 
 class QuestObjectiveConditions(BaseModel):
@@ -53,21 +52,21 @@ class ItemCollection(BaseModel):
     items: dict[str, int | dict[str, int]]
 
 
-class Operation(BaseModel):
+class Operation(BaseModel, extra="allow"):
     name: str
     operation_start_date: datetime
     stop_adding_to_queue_date: datetime
     stop_giving_to_player_date: datetime
     contracts_end_date: datetime
     operation_loot_list: str = Field(alias="operation_lootlist")
-    is_campaign: Optional[int] = None
-    max_drop_count: Optional[int] = None
-    uses_credits: Optional[int] = None
+    is_campaign: int | None = None
+    max_drop_count: int | None = None
+    uses_credits: int | None = None
 
 
 class CapabilityExt(BaseModel, extra="allow"):
-    can_craft_count: ClassVar[bool]
-    paintable: ClassVar[bool]
+    can_craft_count: bool | None = None
+    paintable: bool | None = None
 
 
 class Tool(BaseModel):
@@ -77,115 +76,139 @@ class Tool(BaseModel):
 
 
 class Tag(BaseModel, extra="allow"):
-    can_deal_damage: ClassVar[bool]
-    can_deal_mvm_penetration_damage: ClassVar[bool]
-    can_deal_long_distance_damage: ClassVar[bool]
+    can_deal_damage: bool | None = None
+    can_deal_mvm_penetration_damage: bool | None = None
+    can_deal_long_distance_damage: bool | None = None
 
 
 class PlayerBodyGroup(BaseModel, extra="allow"):
-    hat: ClassVar[bool]
-    head: ClassVar[bool]
-    headphones: ClassVar[bool]
-    grenades: ClassVar[bool]
-    dogtags: ClassVar[bool]
-    backpack: ClassVar[bool]
-    shoes: ClassVar[bool]
-    shoes_socks: ClassVar[bool]
+    hat: bool | None = None
+    head: bool | None = None
+    headphones: bool | None = None
+    grenades: bool | None = None
+    dog_tags: bool | None = Field(alias="dogtags", default=None)
+    backpack: bool | None = None
+    shoes: bool | None = None
+    shoes_socks: bool | None = None
 
 
 class AdditionalHiddenBodyGroup(BaseModel, extra="allow"):
-    hat: ClassVar[bool]
-    head: ClassVar[bool]
-    headphones: ClassVar[bool]
-    grenades: ClassVar[bool]
-    dogtags: ClassVar[bool]
+    hat: bool | None = None
+    head: bool | None = None
+    headphones: bool | None = None
+    grenades: bool | None = None
+    dog_tags: bool | None = Field(alias="dogtags", default=None)
 
 
-class Style(BaseModel):
-    name: str
-    model_player: str
-    additional_hidden_bodygroups: AdditionalHiddenBodyGroup
-    model_player_per_class: dict[str, str]
-    skin_red: bool
-    skin_blu: bool
+class Style(BaseModel, extra="allow"):
+    name: str | None = None
+    player_model: str | None = Field(default=None, alias="model_player")
+    additional_hidden_body_groups: AdditionalHiddenBodyGroup = Field(
+        alias="additional_hidden_bodygroups", default=None
+    )
+    player_model_per_class: dict[str, str] | None = Field(
+        default=None, alias="model_player_per_class"
+    )
+    skin_red: bool | None = None
+    skin_blu: bool | None = None
 
 
-class Visuals(BaseModel):
-    player_bodygroups: PlayerBodyGroup
-    styles: dict[int, Style]
-    custom_particlesystem: dict[str, str]
-    sound_deploy: str
+class Visuals(BaseModel, extra="allow"):
+    player_body_groups: PlayerBodyGroup | None = Field(
+        alias="player_bodygroups", default=None
+    )
+    styles: dict[int, Style] | None = None
+    custom_particle_system: dict[str, str] | None = Field(
+        alias="custom_particlesystem", default=None
+    )
+    sound_deploy: str | None = None
 
 
-class StaticAttr(BaseModel):
-    set_supply_crate_series: int = Field(alias="set supply crate series")
-    hide_crate_series_number: bool = Field(alias="hide crate series numbers")
-    decoded_by_item_def_index: int = Field(alias="decoded by itemdefindex")
-    weapon_allow_inspect: bool
-    item_style_override: bool = Field(alias="item style override")
-    is_winter_case: bool = Field(alias="is winter case")
-    is_marketable: bool = Field(alias="is marketable")
-    is_commodity: bool = Field(alias="is commodity")
-    kill_eater_score_type: int = Field(alias="kill eater score type")
-    kill_eater_score_type_2: int = Field(alias="kill eater score type 2")
-    kill_eater_score_type_3: int = Field(alias="kill eater score type 3")
-    cannot_trade: bool = Field(alias="cannot trade")
-    is_operation_pass: bool
-    style_changes_on_strange_level: int = Field(alias="style changes on strange level")
-    cannot_restore: bool = Field(alias="cannot restore")
-    cannot_giftwrap: bool = Field(alias="cannot giftwrap")
-    always_transmit_so: bool
-    never_craftable: bool = Field(alias="never craftable")
-    deactive_date: int = Field(alias="deactive date")
-    paintkit_proto_def_index: int
-    has_team_color_paintkit: bool
-    limited_quantity_item: bool = Field(alias="limited quantity item")
-    is_giger_counter: bool = Field(alias="is giger counter")
-    min_viewmodel_offset: list[int]
-    inspect_viewmodel_offset: list[int]
-    item_meter_charge_type: int
-    item_meter_charge_rate: int
-    meter_label: str
-    mult_player_movespeed_active: float
-    mod_maxhealth_drain_rate: float
-    energy_weapon_no_ammo: bool
-    energy_weapon_charged_shot: bool
-    energy_weapon_no_hurt_building: bool
-    crits_become_minicrits: bool
-    crit_mod_disabled: bool
-    always_tradable: bool
-    tool_target_item: int = Field(alias="tool target item")
+class StaticAttr(BaseModel, extra="allow"):
+    set_supply_crate_series: int | None = Field(
+        alias="set supply crate series", default=None
+    )
+    hide_crate_series_number: bool | None = Field(
+        alias="hide crate series numbers", default=None
+    )
+    decoded_by_item_def_index: int | None = Field(
+        alias="decoded by itemdefindex", default=None
+    )
+    weapon_allow_inspect: bool | None = None
+    item_style_override: bool | None = Field(alias="item style override", default=None)
+    is_winter_case: bool | None = Field(alias="is winter case", default=None)
+    is_marketable: bool | None = Field(alias="is marketable", default=None)
+    is_commodity: bool | None = Field(alias="is commodity", default=None)
+    kill_eater_score_type: int | None = Field(
+        alias="kill eater score type", default=None
+    )
+    kill_eater_score_type_2: int | None = Field(
+        alias="kill eater score type 2", default=None
+    )
+    kill_eater_score_type_3: int | None = Field(
+        alias="kill eater score type 3", default=None
+    )
+    cannot_trade: bool | None = Field(alias="cannot trade", default=None)
+    is_operation_pass: bool | None = None
+    style_changes_on_strange_level: int | None = Field(
+        alias="style changes on strange level", default=None
+    )
+    cannot_restore: bool | None = Field(alias="cannot restore", default=None)
+    cannot_giftwrap: bool | None = Field(alias="cannot giftwrap", default=None)
+    always_transmit_so: bool | None = None
+    never_craftable: bool | None = Field(alias="never craftable", default=None)
+    deactive_date: int | None = Field(alias="deactive date", default=None)
+    paintkit_proto_def_index: int | None = None
+    has_team_color_paintkit: bool | None = None
+    limited_quantity_item: bool | None = Field(
+        alias="limited quantity item", default=None
+    )
+    is_giger_counter: bool | None = Field(alias="is giger counter", default=None)
+    min_viewmodel_offset: str | None = None
+    inspect_viewmodel_offset: str | None = None
+    item_meter_charge_type: int | None = None
+    item_meter_charge_rate: float | None = None
+    meter_label: str | None = None
+    mult_player_movespeed_active: float | None = None
+    mod_maxhealth_drain_rate: float | None = None
+    energy_weapon_no_ammo: bool | None = None
+    energy_weapon_charged_shot: bool | None = None
+    energy_weapon_no_hurt_building: bool | None = None
+    crits_become_minicrits: bool | None = None
+    crit_mod_disabled: bool | None = None
+    always_tradable: bool | None = None
+    tool_target_item: int | None = Field(alias="tool target item", default=None)
 
 
 class ItemsGameItems(BaseModel, extra="allow"):
     name: str
-    first_sale_date: ClassVar[datetime]
-    prefab: ClassVar[str]
-    baseitem: ClassVar[int]
-    capabilities: ClassVar[CapabilityExt]
-    equip_regions: ClassVar[dict[str, int]]
-    propername: ClassVar[bool]
-    min_ilevel: ClassVar[int]
-    max_ilevel: ClassVar[int]
-    hidden: ClassVar[str]
-    inspect_panel_dist: ClassVar[int]
-    item_name: ClassVar[str]
-    item_type_name: ClassVar[str]
-    item_slot: ClassVar[str]
-    item_quality: ClassVar[str]
-    image_inventory: ClassVar[str]
-    image_inventory_size_w: ClassVar[int]
-    image_inventory_size_h: ClassVar[int]
-    craft_class: ClassVar[str]
-    craft_material_type: ClassVar[str]
-    player_model: ClassVar[str] = Field(alias="model_player")
-    attach_to_hands: ClassVar[int]
-    drop_type: ClassVar[str]
-    used_by_classes: ClassVar[dict[str, bool]]
-    mouse_pressed_sound: ClassVar[str]
-    drop_sound: ClassVar[str]
-    tags: ClassVar[Tag]
-    ad_text: ClassVar[str]
-    default_skin: ClassVar[int]
-    static_attrs: ClassVar[StaticAttr]
-    xifier_class_remap: ClassVar[str]
+    first_sale_date: str | None = None
+    prefab: str | None = None
+    baseitem: int | None = None
+    capabilities: CapabilityExt | None = None
+    equip_regions: dict[str, int] | None = None
+    proper_name: bool | None = Field(alias="propername", default=None)
+    min_item_level: int | None = Field(alias="min_ilevel", default=None)
+    max_item_level: int | None = Field(alias="max_ilevel", default=None)
+    hidden: bool | None = None
+    inspect_panel_dist: int | None = None
+    item_name: str | None = None
+    item_type_name: str | None = None
+    item_slot: str | None = None
+    item_quality: str | None = None
+    image_inventory: str | None = None
+    image_inventory_size_w: int | None = None
+    image_inventory_size_h: int | None = None
+    craft_class: str | None = None
+    craft_material_type: str | None = None
+    player_model: str | None = Field(alias="model_player", default=None)
+    attach_to_hands: int | None = None
+    drop_type: str | None = None
+    used_by_classes: dict[str, bool | str] | None = None
+    mouse_pressed_sound: str | None = None
+    drop_sound: str | None = None
+    tags: Tag | None = None
+    ad_text: str | None = None
+    default_skin: int | None = None
+    static_attrs: StaticAttr | None = None
+    xifier_class_remap: str | None = None
